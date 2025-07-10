2a2b5a4b5e71e5438d685c6db11d5e1ae15ead9f
vsaint1 <vsantos1.dev@gmail.com>
2025-07-10 02:27:49 -0300

    fix application tests config

--- src/test/java/br/com/crm/beauty/CrmBeautyApplicationTests.java
+++ src/test/java/br/com/crm/beauty/CrmBeautyApplicationTests.java
@@ -2,9 +2,11 @@ package br.com.crm.beauty;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest(classes = CrmBeautyApplication.class)
+@ActiveProfiles("test")
 class CrmBeautyApplicationTests {
 
 	@Test
