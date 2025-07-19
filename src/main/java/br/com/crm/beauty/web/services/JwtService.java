c57d9f40af00784140c9d6a7e0949168fd87ef14
vsantos1 <vsantos1.dev@gmail.com>
2025-07-18 23:47:03 -0300

    decode jwt and refreshtoken

--- src/main/java/br/com/crm/beauty/web/services/JwtService.java
+++ src/main/java/br/com/crm/beauty/web/services/JwtService.java
@@ -2,8 +2,11 @@ package br.com.crm.beauty.web.services;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -14,12 +17,14 @@ import br.com.crm.beauty.web.dtos.UserDto;
 @Service
 public class JwtService {
 
-    private JwtEncoder jwtEncoder;
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
 
     private final long EXPIRATION_TIME = 36000L; // 10 hours in seconds
 
-    public JwtService(JwtEncoder jwtEncoder) {
+    public JwtService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
         this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
     }
 
     public JwtTokenDto generate(UserDto user) {
@@ -38,6 +43,33 @@ public class JwtService {
 
         return new JwtTokenDto(jwt.getTokenValue(), createdAt, expiration);
 
+    }
+
+    public String decodeSubject(String token) {
+        Jwt jwt = jwtDecoder.decode(token.substring(7));
+        var email = jwt.getSubject();
+        return email;
+    }
+
+    public JwtTokenDto refreshToken(String token) {
+        Jwt jwt = jwtDecoder.decode(token);
+        String email = jwt.getSubject();
+        List<String> roles = jwt.getClaimAsStringList("roles");
+
+        var createdAt = LocalDateTime.now();
+        var expiration = createdAt.plusSeconds(EXPIRATION_TIME);
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("crm_beautysalon")
+                .issuedAt(createdAt.toInstant(ZoneOffset.UTC))
+                .expiresAt(expiration.toInstant(ZoneOffset.UTC))
+                .subject(email)
+                .claim("roles", roles)
+                .build();
+
+        var newJwt = jwtEncoder.encode(JwtEncoderParameters.from(claims));
+
+        return new JwtTokenDto(newJwt.getTokenValue(), createdAt, expiration);
     }
 
 }
